a426ee6d3e1fc292752c5cbd80be7802ffbcf18f
Bryan Vela <55074395+bdvela@users.noreply.github.com>
2024-09-21 14:52:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -39,14 +39,14 @@ btnCarta.addEventListener('click', () => {
 btnImagen.addEventListener('click', () => {
     console.log('Botón de imagen presionado');
     gsap.to(modalCarta, { opacity: 0, duration: 0, onComplete: () => (modalCarta.style.display = 'none') });
-    gsap.to('body', {
-        backgroundImage: "url('./yellow-flower.gif')", // Cambia la imagen de fondo
-        backgroundSize: '100% auto', // Asegura que la imagen ocupe todo el ancho de la pantalla
-        backgroundRepeat: 'no-repeat', // Evita que se repita la imagen
-        backgroundPosition: 'center center', // Centra la imagen horizontalmente
-        duration: 0, // Duración de la animación
-        ease: 'power2.inOut',
-    });
+    // gsap.to('body', {
+    //     backgroundImage: "url('./yellow-flower.gif')", // Cambia la imagen de fondo
+    //     backgroundSize: '100% auto', // Asegura que la imagen ocupe todo el ancho de la pantalla
+    //     backgroundRepeat: 'no-repeat', // Evita que se repita la imagen
+    //     backgroundPosition: 'center center', // Centra la imagen horizontalmente
+    //     duration: 0, // Duración de la animación
+    //     ease: 'power2.inOut',
+    // });
     modalImagen.style.display = 'flex'; // Mostrar el modal de la imagen
     gsap.fromTo(modalImagen, { opacity: 0 }, { opacity: 1, duration: 0.5 });
 });
@@ -54,24 +54,25 @@ btnImagen.addEventListener('click', () => {
 // Cerrar el modal de la carta
 closeCarta.addEventListener('click', () => {
     console.log('Cerrando carta');
-    gsap.to('body', {
-        backgroundImage: 'none', // Cambia la imagen de fondo
-        backgroundSize: '100% auto', // Asegura que la imagen ocupe todo el ancho de la pantalla
-        backgroundRepeat: 'no-repeat', // Evita que se repita la imagen
-        backgroundPosition: 'center center', // Centra la imagen horizontalmente
-        duration: 0, // Duración de la animación
-        ease: 'power2.inOut',
-    });
+    // gsap.to('body', {
+    //     backgroundImage: 'none', // Cambia la imagen de fondo
+    //     backgroundSize: '100% ',
+    //     backgroundHeight: '100%', // Asegura que la imagen ocupe todo el ancho de la pantalla
+    //     backgroundRepeat: 'no-repeat', // Evita que se repita la imagen
+    //     backgroundPosition: 'center center', // Centra la imagen horizontalmente
+    //     duration: 0, // Duración de la animación
+    //     ease: 'power2.inOut',
+    // });
     gsap.to(modalCarta, { opacity: 0, duration: 0.5, onComplete: () => (modalCarta.style.display = 'none') });
 });
 
 // Cerrar el modal de la imagen
 closeImagen.addEventListener('click', () => {
     console.log('Cerrando imagen');
-    gsap.to('body', {
-        backgroundImage: 'none', // Cambia la imagen de fondo
-        duration: 0, // Duración de la animación
-        ease: 'power2.inOut',
-    });
+    // gsap.to('body', {
+    //     backgroundImage: 'none', // Cambia la imagen de fondo
+    //     duration: 0, // Duración de la animación
+    //     ease: 'power2.inOut',
+    // });
     gsap.to(modalImagen, { opacity: 0, duration: 0.5, onComplete: () => (modalImagen.style.display = 'none') });
 });
